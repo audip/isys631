bba30251759d4d696cc5fae278e590e6646c4244
Gaurav Diwanji <gauravd@tamu.edu>
2017-04-01 19:25:01 -0500

    search_results_angular.html working based on ficed JSON

--- scripts/angularController.js
+++ scripts/angularController.js
@@ -10,21 +10,21 @@ app.service('appDataService', function(){
 	'success': true,
 	'search': [{
 						'doctor_id': 3546,
-						'photo_url': 'http://www.lolcorps.com/view/image/1445/programmer%20no%20life',
+						'photo_url': './assets/images/custom/doctor_image_1.jpg',
 						'name': 'Gaurav Diwanji',
 						'qualification': 'ENT Specialist',
 						'experience': 5,
 						'address': '4302 College Main St. Apt 319, Bryan, Tx - 77801',
-						'rating': 3.96
+						'rating': 5
 					},
 					{
 						'doctor_id': 3547,
-						'photo_url': 'http://www.lolcorps.com/view/image/1445/programmer%20no%20life',
+						'photo_url': './assets/images/custom/doctor_image_1.jpg',
 						'name': 'Lijun Wang',
 						'qualification': 'Gyneacologist',
 						'experience': 3,
 						'address': 'Scnadia, College Station, Tx - 77843',
-						'rating': 4.30
+						'rating': 4
 					}],
 	'error': ''
     };
@@ -77,23 +77,27 @@ app.factory('dataFactory', ['$http','$q','appDataService', function($http,$q,app
     return dataFactory;
 }]);
                 
-app.config(function($routeProvider) {
-        $routeProvider
-        .when("/search_results", {
-            controller: 'search_resultsCntrl',
-            templateUrl : "./partials/search_results.html"
-        })
-        .otherwise({
-            controller: 'search_resultsCntrl',
-            templateUrl : "./partials/search_results.html"
-        });
-    });
+//app.config(function($routeProvider) {
+//        $routeProvider
+//        .when("/search_results", {
+//            controller: 'search_resultsCntrl',
+//            templateUrl : "./partial-search_results.html"
+//        })
+//        .otherwise({
+//            controller: 'search_resultsCntrl',
+//            templateUrl : "./partial-search_results.html"
+//        });
+//    });
 
 app.controller('search_resultsCntrl', ['$scope', '$location','appDataService','dataFactory',function($scope, $location,appDataService,dataFactory) {
     
-    $scope.serchTerm = appDataService.getSearchTerm();
+    $scope.searchTerm = appDataService.getSearchTerm();
     
     $scope.searchResult = appDataService.getSearchResult();
+    
+    if ($scope.searchResult.success == true){
+    $scope.doctorList = $scope.searchResult.search;  
+    };
     
     $scope.buttonClick = function () {
     };
